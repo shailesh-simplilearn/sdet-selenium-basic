bb1fa740c9b33449b68db679e9b351b3f1cda7d4
shailesh-simplilearn <80203377+shailesh-simplilearn@users.noreply.github.com>
2022-04-04 23:08:09 +0530

    test commit

--- src/com/util/ReportGenerator.java
+++ src/com/util/ReportGenerator.java
@@ -9,8 +9,10 @@ import com.relevantcodes.extentreports.ExtentTest;
 public class ReportGenerator {
 	String pattern = "MM_dd_yyyy_hh_mm";
 	SimpleDateFormat format = new SimpleDateFormat(pattern);
+	
 	public ExtentReports reports = new ExtentReports
 			(System.getProperty("user.dir")+"/reports/test-report_"+ format.format(new Date())+".html");
+
 	public ExtentTest test = reports.startTest("First-Test");
 	
 }
